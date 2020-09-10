062c36dee6d223a3b9b1b178131c50c5bcd846eb
ilianachakarova <iliana1212@abv.bg>
2020-09-10 15:45:48 +0200

    Added Swagger2

--- src/main/java/com/example/redditclone/config/SecurityConfig.java
+++ src/main/java/com/example/redditclone/config/SecurityConfig.java
@@ -36,6 +36,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/api/auth/**")
                 .permitAll()
+                .antMatchers("/v2/api-docks",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**")
+                .permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
