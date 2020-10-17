5b2bf341936b323619b3041c5025e26869282d98
ilianachakarova <iliana1212@abv.bg>
2020-10-17 23:14:23 +0200

    magic

--- src/main/java/com/example/redditclone/config/WebConfig.java
+++ src/main/java/com/example/redditclone/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://angularspringblog.herokuapp.com")
                 .allowedMethods("*")
                 .maxAge(3600L)
                 .allowedHeaders("*")
