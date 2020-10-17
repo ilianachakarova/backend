06d0f13dae9f7587f0f8657b86b379d18bfcf962
ilianachakarova <iliana1212@abv.bg>
2020-10-17 23:19:48 +0200

    magic

--- src/main/java/com/example/redditclone/config/WebConfig.java
+++ src/main/java/com/example/redditclone/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.example.redditclone.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,17 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://angularspringblog.herokuapp.com")
-                .allowedMethods("*")
-                .maxAge(3600L)
-                .allowedHeaders("*")
-                .exposedHeaders("Authorization")
-                .exposedHeaders("Access-Control-Allow-Origin")
-                .allowCredentials(true);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("https://angularspringblog.herokuapp.com")
+//                .allowedMethods("*")
+//                .maxAge(3600L)
+//                .allowedHeaders("*")
+//                .exposedHeaders("Authorization")
+//                .exposedHeaders("Access-Control-Allow-Origin")
+//                .allowCredentials(true);
+//    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
